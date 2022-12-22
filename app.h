cae130bf32628969bc25a3e350bb53a02776f05b
momen10 <93386244+momen10@users.noreply.github.com>
2022-12-22 05:37:19 +0200

    Create app.h

---
+++ app.h
@@ -0,0 +1,17 @@
+ /******************************************************************************
+ *
+ * Module: APP
+ *
+ * File Name: App.h
+ *
+ * Description: header file for the card module
+ *
+ * Author: Mo'men Ahmed
+ *
+ *******************************************************************************/
+#ifndef APP_H_
+#define APP_H_
+
+void appStart(void);
+
+#endif /* APP_H_ */
