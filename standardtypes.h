d39ab6676c3f7940372883874ea4fd2ed1658c8d
momen10 <93386244+momen10@users.noreply.github.com>
2022-12-22 05:36:45 +0200

    Create standardtypes.h

---
+++ standardtypes.h
@@ -0,0 +1,16 @@
+/*
+ * standard_types.h
+ *
+ *  Created on: Dec 12, 2022
+ *      Author: Mo'men
+ */
+
+#ifndef STANDARD_TYPES_H_
+#define STANDARD_TYPES_H_
+
+typedef unsigned char uint8_t;
+typedef signed char sint8_t;
+typedef unsigned int uint32_t;
+
+
+#endif /* STANDARD_TYPES_H_ */
